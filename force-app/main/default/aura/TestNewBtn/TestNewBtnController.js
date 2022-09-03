719897cf15ff4c55c19f7f181d7da1ed0d0d3aef
Vikas Kumar Tank <vikaskumar171020@gmail.com>
2022-09-03 21:25:00 +0530

    Update

--- force-app/main/default/aura/TestNewBtn/TestNewBtnController.js
+++ force-app/main/default/aura/TestNewBtn/TestNewBtnController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
-
+    closeModel : function(component, event, helper) {
+        component.set("v.isOpen", false);
+        if (component.get("v.recordId") != null) {
+            var navEvt = $A.get("e.force:navigateToSObject");
+            navEvt.setParams({
+                "recordId": component.get("v.recordId"),
+                "slideDevName": "detail"
+            });
+            navEvt.fire();
+        } else {
+            window.history.back();
+        }
     }
+
+    
 })
